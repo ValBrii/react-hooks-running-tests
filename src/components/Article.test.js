b3c513c76bbae7cb206f5e0c5aa7c42eb0aa91ce
Valerie <britneyvalerie85@gmail.com>
2025-01-13 19:15:10 +0300

    changes added

---
+++ src/components/Article.test.js
@@ -0,0 +1,8 @@
+test("displays the text 'please pass this test'", () => {
+    render(<Article />);
+  
+    // add this line
+    screen.debug();
+  
+    expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+  });
